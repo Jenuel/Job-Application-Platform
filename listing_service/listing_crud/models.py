367c5a2db94ea98f1d6f2446ac5177d1e6bb15bc
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-12-19 22:19:38 +0800

    created the Job model

--- listing_service/listing_crud/models.py
+++ listing_service/listing_crud/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max)
+    description = models.TextField()
+    status = models.CharField(max_length=20, default='Pending')
+    acc_id = models.IntegerField()
