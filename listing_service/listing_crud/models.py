3962bb226a7a4d38ba421431c11e36cbb1d08af7
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-12-20 22:02:45 +0800

    modified the job model

--- listing_service/listing_crud/models.py
+++ listing_service/listing_crud/models.py
@@ -4,5 +4,7 @@ from django.db import models
 class Job(models.Model):
     title = models.CharField(max)
     description = models.TextField()
-    status = models.CharField(max_length=20, default='Pending')
+    status = models.CharField(max_length=20, default='Pending') #for the vendor and checking of availability
+    rating = models.CharField()
+    wage = models.IntegerField()
     acc_id = models.IntegerField()
