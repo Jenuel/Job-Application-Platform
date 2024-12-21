d2834049ac11650658bcf5180734b41c1591b0e3
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-12-21 14:46:06 +0800

    created the implementation for getting jobs

--- listing_service/listing_crud/views.py
+++ listing_service/listing_crud/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import JobSerializer
+from .models import Job
 
 # Create your views here.
+def get_jobs(request):
+    jobs =  Job.objects.all()
+    serializer = JobSerializer(jobs, many=True)
+    return Response(serializer.data, status=200)
+  
+
