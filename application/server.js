6dfaf4d2a72e7b6116c76e5ab092209f73ddc09f
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-12-29 17:24:50 +0800

    Created the server file

---
+++ application/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mysql2 from 'mysql2';
+
+const app = express();
+const port = 3000;
+
+const db = mysql2.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jobapplication'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
