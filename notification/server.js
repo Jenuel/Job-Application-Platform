b4f2bb2d63d3633147b41c67f50f73e132c4ad6f
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2025-01-03 15:39:43 +0800

    made the boilerplate code for the server file of the notification server

---
+++ notification/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json()); 
+
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
