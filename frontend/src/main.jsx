a9c1e98c19f1b508cc84008fd39d332ef868b9ab
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2025-01-13 22:43:32 +0800

    Add registration route and import RegistrationPage component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,28 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LoginPage from './pages/LoginPage'
+import LoginPage from './pages/LoginPage';
 import JobPage from './pages/JobPage';
 import JobDetailsPage from './pages/JobDetailsPage';
-
+import RegistrationPage from './pages/RegistrationPage'; 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <LoginPage /> //should be landing page but login for now... also add an error element (404 page)
+    element: <LoginPage />, 
+  },
+  {
+    path: '/register',
+    element: <RegistrationPage />,
   },
   {
     path: '/jobs',
-    element: <JobPage />
+    element: <JobPage />,
   },
   {
     path: '/jobs/:jobId',
-    element: <JobDetailsPage />
-  }
-])
+    element: <JobDetailsPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
