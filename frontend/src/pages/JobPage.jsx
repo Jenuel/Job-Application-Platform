0909828fe2ed3f1ace6e0b7049519d6d5d84a4d0
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2025-01-15 23:27:56 +0800

    Create buttons for applying and creating a job

--- frontend/src/pages/JobPage.jsx
+++ frontend/src/pages/JobPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom"; // Import Link
 import jobTestData from "./testData"; // Import the test data
 
 function JobPage() {
   return (
     <div>
+      <Button variant="primary" style={{ marginBottom: "1rem" }}> Create Job </Button>
       {jobTestData.map((job) => (
         <Link to={`/jobs/${job.id}`} key={job.id} style={{ textDecoration: "none" }}>
           <Card style={{ marginBottom: "1rem", cursor: "pointer" }}>
@@ -18,6 +19,7 @@ function JobPage() {
               <Card.Text>
                 <strong>Wage:</strong> ${job.wage} | <strong>Account ID:</strong> {job.acc_id}
               </Card.Text>
+              <Button variant="primary">Apply</Button>
               <Card.Footer className="text-muted">
                 Created At: {new Date(job.created_at).toLocaleString()}
               </Card.Footer>
