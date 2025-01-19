d9a3872bf3ab14a770ca3bc4a9c9a644669c2fe8
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2025-01-19 13:08:13 +0800

    Implemented cors

--- authentication/index.js
+++ authentication/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import authRoutes from './routes/authRoutes.js';
 
 dotenv.config()
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 //routes
