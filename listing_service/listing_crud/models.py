edaf0ec7f179288e22818a255603db4b599ca261
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2025-02-07 13:28:24 +0800

    Fix Job model field definitions by adding max_length constraints for title and rating

--- listing_service/listing_crud/models.py
+++ listing_service/listing_crud/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
-    title = models.CharField(max)
+    title = models.CharField(max_length=255)
     description = models.TextField()
-    rating = models.CharField()
+    rating = models.CharField(max_length=10)
     wage = models.IntegerField()
     status = models.CharField(max_length=20, default='Pending') #for the vendor and checking of availability
     acc_id = models.IntegerField()
