e02dfd9595a364805f4250397a6a46d4525d16a8
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2025-02-21 13:59:31 +0800

    Renamed the main server file of authentication service

--- authentication/server.js
+++ authentication/server.js
@@ -10,13 +10,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-//routes
 app.use("/auth", authRoutes)
 
-//database connection
 mongoose.connect(process.env.DB_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
-        console.log("Listening on port,", process.env.PORT || 3000)
+        console.log("Listening on port,", process.env.PORT || 4000)
     })
   })
